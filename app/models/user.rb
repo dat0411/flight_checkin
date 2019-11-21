c498148f476e92d8243ca5bef01f7471d5f8cb93
Raghu Betina <raghu@firstdraft.com>
2019-11-21 20:09:42 +0000

    Validate presence of users.phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :phone, :presence => true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
