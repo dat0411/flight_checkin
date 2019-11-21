22d6d0d52a36bed1fffb29006366965e072b1191
Raghu Betina <raghu@firstdraft.com>
2019-11-21 20:10:03 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "flight_tables#index"
   # Routes for the Flight table resource:
 
